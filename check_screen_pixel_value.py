59be9d14be2a65d056a78329fb253006832dd53f
fabian57fabian <fabian_57@yahoo.it>
2020-03-15 01:40:28 +0100

    Added script to fast check one screenshot (x,y) point value

---
+++ check_screen_pixel_value.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+from PIL import Image
+from adb_connector import adb_screen_getpixels
+
+screenshot_path = 'screens/samsung_s10e/Screenshot_20200315-002432_Archero.png'
+x,y = 0.194444, 0.675676
+
+
+width = 1080
+height = 2220
+x, y = x * width, y * height
+# print("Taking a screenshot...")
+# frame = adb_screen_getpixels()
+
+pixval = []
+with Image.open(screenshot_path, 'r') as im:
+    frame = np.array(im.getdata())
+pos = int(y * width + x)
+pixel = frame[pos]
+print("pixel:")
+print(pixel)
