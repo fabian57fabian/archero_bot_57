456561398916f9f5766e6ad6ebf10b03ea0ba3ab
fabian57fabian <fabian_57@yahoo.it>
2020-04-05 14:41:58 +0200

    Added UI ask for interface

--- static_bot_cave.py
+++ static_bot_cave.py
@@ -18,6 +18,21 @@ def get_start_lvl_from_args():
 
 
 while True:
-    engine = CaveEngine()
-    engine.currentLevel = get_start_lvl_from_args()
-    engine.start_infinite_play()
+    answer = input("Do you want to use the interface? (y/n):")
+    if answer == 'y' or answer == 'yes':
+        from PyQt5 import QtWidgets
+        from GameController.GameControllerView import GameControllerWindow
+        from GameController.GameControllerModel import GameControllerModel
+        from GameController.GameControllerController import GameControllerController
+
+        app = QtWidgets.QApplication(sys.argv)
+        model = GameControllerModel()
+        controller = GameControllerController(model)
+        ui = GameControllerWindow(model, controller)
+        model.load_data()
+        ui.show()
+        sys.exit(app.exec_())
+    else:
+        engine = CaveEngine()
+        engine.currentLevel = get_start_lvl_from_args()
+        engine.start_infinite_play()
