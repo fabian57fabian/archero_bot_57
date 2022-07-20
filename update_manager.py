2880a5ec5627da38b8067039a39a2133ea83a44a
AdminZero <meepstaff@gmail.com>
2022-07-20 02:11:51 -0400

    Input UPDATE_MANAGER changes by Fabian57Fabian

--- update_manager.py
+++ update_manager.py
@@ -4,6 +4,7 @@ from requests import get
 class UpdatesManager:
     def __init__(self):
         self.request_url = "http://217.160.188.167:3000/api/archero_updates"
+        self.CURRENT_VERSION = "v2"
 
     def get_my_update_code(self):
         # better if have hash of last commit.
@@ -11,7 +12,7 @@ class UpdatesManager:
 
     def ask_for_updates(self):
         try:
-            params = {"my_app_update": self.get_my_update_code(), "version": "v1"}
+            params = {"my_app_update": self.get_my_update_code(), "version": self.CURRENT_VERSION}
             r = get(url=self.request_url, json=params)
             if r.ok:
                 data = r.json()
@@ -25,8 +26,14 @@ class UpdatesManager:
                             if str(res) == "NO_NEW_UPDATES":
                                 print("Updates checked. All right.")
                                 return False
-                            else:
+                            elif str(res) == "NEW_UPDATE_AVAILABLE":
+                                server_ver = "V99"
+                                if "version" in data: server_ver = data["version"]
+                                print("Server have a different version: {}".format(server_ver))
                                 return True
+                            else:
+                                #unknown
+                                return False
             else:
                 print("Unable to request for updates, got {}".format(r.status_code))
                 return False
